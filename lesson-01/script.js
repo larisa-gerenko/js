4a06e6e8e78b38e7862f15cb50df894077f1b277
Larisa <larisavalgutova@gmail.com>
2020-05-06 15:37:33 +0300

    Вторая задача

---
+++ lesson-01/script.js
@@ -0,0 +1,15 @@
+// Первая задача
+
+/* let allSquare = 10;
+let metrAllSquare = allSquare * 100; // Общая площадь в метрах
+let garden = 15 * 25; // Площадь одной грядки
+let amount = parseInt(metrAllSquare / garden); // Количество целых грядок
+let freeSquare = metrAllSquare - amount * garden;
+console.log(freeSquare); */
+
+//Вторая задача
+
+// let sm = 15 * 100; // Общая площадь в см2
+// let buzy = 600;
+// let free = sm - buzy;
+// console.log(free); // Площадь остатка
