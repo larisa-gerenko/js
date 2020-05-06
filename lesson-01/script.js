2e008f78d55eeb120ef1dd9fb067eb4c986edd91
Larisa <larisavalgutova@gmail.com>
2020-05-06 18:15:10 +0300

    Задача пять

--- lesson-01/script.js
+++ lesson-01/script.js
@@ -23,14 +23,31 @@ console.log(freeSquare); */
 
 // Четвертая задача
 
-let number = 50;
-let x = 14;
-let y = 156;
+// let number = 50;
+// let x = 14;
+// let y = 156;
 
-if (Math.abs(x - number) > Math.abs(y - number)) {
-  alert("Ближайшее к заданному числу: " + y);
-} else if (Math.abs(x - number) < Math.abs(y - number)) {
-  alert("Ближайшее к заданному числу: " + x);
+// if (Math.abs(x - number) > Math.abs(y - number)) {
+//   alert("Ближайшее к заданному числу: " + y);
+// } else if (Math.abs(x - number) < Math.abs(y - number)) {
+//   alert("Ближайшее к заданному числу: " + x);
+// } else {
+//   alert("Оба числа на одинаковом отдалении");
+// }
+
+// Пятая задача
+
+let Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz;
+let AB = (Bx - Ax) ** 2 + (By - Ay) ** 2 + (Bz - Az) ** 2;
+let AC = (Cx - Ax) ** 2 + (Cy - Ay) ** 2 + (Cz - Az) ** 2;
+let BC = (Cx - Bx) ** 2 + (Cy - By) ** 2 + (Cz - Bz) ** 2;
+
+if (AB > AC && AB > BC && AB == AC + BC) {
+  alert("Прямоугольник является прямоугольным");
+} else if (AC > AB && AC > BC && AC == AB + BC) {
+  alert("Прямоугольник является прямоугольным");
+} else if (BC > AB && BC > AC && BC == AB + AC) {
+  alert("Прямоугольник является прямоугольным");
 } else {
-  alert("Оба числа на одинаковом отдалении");
+  alert("Прямоугольник не является прямоугольным");
 }
