c13fbdc43751136b004cc9ffefd36f7f11be1d36
Larisa <larisavalgutova@gmail.com>
2020-05-15 18:25:54 +0300

    Задача 1

---
+++ lesson-03/script.js
@@ -0,0 +1,12 @@
+// Задача 1
+
+let arrA = [12, 4, 3, 10, 1, 20];
+let arrB = [-3, -7, -100, -33];
+
+let arrC1 = arrB.concat(arrA);
+let arrC2 = arrA.concat(arrB);
+
+console.log(arrC1);
+console.log(arrC2);
+
+// Задача 2
