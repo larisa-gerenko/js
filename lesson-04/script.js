63df91e02e64bd77cf8ee94f2c029a798b610652
Larisa <larisavalgutova@gmail.com>
2020-05-15 23:35:48 +0300

    Задача 2,3,4

---
+++ lesson-04/script.js
@@ -0,0 +1,14 @@
+//Задача 1
+
+// let str = "154";
+// let sum
+
+
+// console.log(Number(str[0]) + Number(str[1]) + Number(str[2]));
+
+// for (i = 0; i < str.length; i++) {
+
+// }
+
+// Задача 2
+
