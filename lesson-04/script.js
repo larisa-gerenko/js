58c75415f237ef614f8681de9f8b78106b263d7f
Larisa <larisavalgutova@gmail.com>
2020-05-18 22:51:38 +0300

    Задача 3

--- lesson-04/script.js
+++ lesson-04/script.js
@@ -17,3 +17,36 @@
 // str = str.replace(new RegExp(symbol, "g"), symbol + symbol);
 
 // console.log(str);
+
+// Задача 3
+
+let password = "wertffffffffff444";
+
+let search = password.match(/./gi);
+let number = password.match(/\d/gi);
+
+if (search.length >= 9) {
+  if (/[a-z]/g.test(password)) {
+    if (/[A-Z]/g.test(password)) {
+      if (number.length > 2) {
+        if (/\$|\#|\^|\%/.test(password)) {
+          console.log("Введенный пароль удовлетворяет условиям сложности");
+        } else {
+          console.log(
+            "Пароль должен содержать один из неалфавитных символов: $, #, %, ^"
+          );
+        }
+      } else {
+        console.log("Пароль должен содержать более двух цифр");
+      }
+    } else {
+      console.log(
+        "Пароль должен содержать английские буквы верхнего регистра;"
+      );
+    }
+  } else {
+    console.log("Пароль должен содержать английские буквы нижнего регистра;");
+  }
+} else {
+  console.log("Пароль должен содержать от 9 символов");
+}
