e12bed14c8175fc7aa77bf23d3b3b2f82487d0cf
Larisa <larisavalgutova@gmail.com>
2020-05-18 12:01:30 +0300

    Задача 2

--- lesson-04/script.js
+++ lesson-04/script.js
@@ -1,10 +1,19 @@
 // Задача 1
 
-let str = "12345";
-let sum = 0;
+// let str = "12345";
+// let sum = 0;
 
-for (i = 0; i < str.length; i++) {
-  sum += Number(str[i]);
-}
+// for (i = 0; i < str.length; i++) {
+//   sum += Number(str[i]);
+// }
 
-console.log(sum);
+// console.log(sum);
+
+// Задача 2
+
+// let str = "Лариса";
+// let symbol = "а";
+
+// str = str.replace(new RegExp(symbol, "g"), symbol + symbol);
+
+// console.log(str);
