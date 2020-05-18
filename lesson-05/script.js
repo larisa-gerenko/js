33fd15cb5582ff84e7d2498a0661842607757ca0
Larisa <larisavalgutova@gmail.com>
2020-05-18 23:09:15 +0300

    Задача 1-2

---
+++ lesson-05/script.js
@@ -0,0 +1,35 @@
+// Задача 1
+
+// let a = [5, 7, 10, 8];
+// let b = [5, 7, 8, 3];
+
+// function compare(arr1, arr2) {
+//   if (arr1.length != arr2.length) {
+//     return false;
+//   }
+
+//   for (i = 0; i < arr1.length; i++) {
+//     if (arr1[i] != arr2[i]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// let res = compare(a, b);
+
+// Задача 2
+
+function range(start, end, step) {
+  const arr = [];
+
+  for (let i = start; i <= end; i = i + step) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
+let result = range(5, 50, 2);
+console.log(result);
