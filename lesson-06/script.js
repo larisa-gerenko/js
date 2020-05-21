b60fa2cfe48dc0c1588060c908231f29d095a809
Larisa <larisavalgutova@gmail.com>
2020-05-21 18:07:15 +0300

    Задача 6.1

---
+++ lesson-06/script.js
@@ -0,0 +1,18 @@
+// Задача 1
+
+// let a = {
+//   name: "static",
+//   count: 0,
+// };
+
+// function foo(obj, id, number) {
+//   const res = [];
+//   for (let i = 0; i < number; i++) {
+//     let copyObj = { ...obj };
+//     copyObj[id] = i;
+//     res.push(copyObj);
+//   }
+//   return res;
+// }
+
+// console.log(foo(a, "count", 10));
