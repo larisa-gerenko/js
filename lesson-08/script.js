2ec9d900beaa2153e51ced4840d3551343d43680
Larisa <larisavalgutova@gmail.com>
2020-06-01 23:28:17 +0300

    Задача 8.1 и 8.2

---
+++ lesson-08/script.js
@@ -0,0 +1,15 @@
+// Задача 1
+
+// function colorChanger(el) {
+//   if (el.style.backgroundColor) {
+//     el.style.backgroundColor = "";
+//   } else {
+//     el.style.backgroundColor = "green";
+//   }
+// }
+
+// Задача 2
+
+function counter(el) {
+  el.textContent++;
+}
