aa103781ee24bbd227a34d35145666ea4b8de14d
Larisa <larisavalgutova@gmail.com>
2020-06-13 16:37:20 +0300

    Задача 10.3

--- lesson-10/script.js
+++ lesson-10/script.js
@@ -43,45 +43,68 @@
 
 // Задача 2
 
-function Human(name, age, gender, hobby) {
-  this.name = name;
-  this.age = age;
-  this.gender = gender;
-  this.hobby = hobby;
+// function Human(name, age, gender, hobby) {
+//   this.name = name;
+//   this.age = age;
+//   this.gender = gender;
+//   this.hobby = hobby;
 
-  this.humanDescription = function () {
-    return (
-      "Человек: " +
-      name +
-      " Возраст: " +
-      age +
-      " Пол: " +
-      gender +
-      " Интересы: " +
-      hobby
-    );
+//   this.humanDescription = function () {
+//     return (
+//       "Человек: " +
+//       name +
+//       " Возраст: " +
+//       age +
+//       " Пол: " +
+//       gender +
+//       " Интересы: " +
+//       hobby
+//     );
+//   };
+// }
+
+// function Student(name, age, gender, hobby, university) {
+//   Human.call(this, name, age, gender, hobby);
+//   this.university = university;
+
+//   this.studentDescription = function () {
+//     return (
+//       "Человек: " +
+//       name +
+//       " Возраст: " +
+//       age +
+//       " Пол: " +
+//       gender +
+//       " Интересы: " +
+//       hobby +
+//       " Обучается в: " +
+//       university
+//     );
+//   };
+// }
+
+// let student = new Student("Иван", 20, "М", "музыка, танцы", "ИТМО");
+// console.log(student.studentDescription());
+
+// Задача 3
+
+function User() {
+  this.toString = function () {
+    return "Аноним";
   };
 }
 
-function Student(name, age, gender, hobby, university) {
-  Human.call(this, name, age, gender, hobby);
-  this.university = university;
+User.new = function (data) {
+  const user = new User();
+  user.name = data.name;
+  user.age = data.age;
 
-  this.studentDescription = function () {
-    return (
-      "Человек: " +
-      name +
-      " Возраст: " +
-      age +
-      " Пол: " +
-      gender +
-      " Интересы: " +
-      hobby +
-      " Обучается в: " +
-      university
-    );
+  user.toString = function () {
+    return "Имя пользователя " + this.name + ", Возраст " + this.age;
   };
-}
 
-let student = new Student("Иван", 20, "М", "музыка, танцы", "ИТМО");
-console.log(student.studentDescription());
+  return user;
+};
+
+const user1 = new User();
+const user2 = User.new({ name: "Вася", age: "34" });
