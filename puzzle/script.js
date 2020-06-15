99f57101e00db743fa2b29138cd375da9a1befbd
Larisa <larisavalgutova@gmail.com>
2020-06-16 00:35:29 +0300

    puzzle

--- puzzle/script.js
+++ puzzle/script.js
@@ -1,14 +1,43 @@
-// function drag(ev) {
-//   console.log(ev);
-// }
+const img = [
+  "https://media.gettyimages.com/photos/cat-headphones-wearing-sunglasses-relaxing-in-the-grass-picture-id512291806?s=2048x2048",
+  "https://media.gettyimages.com/vectors/cute-cartoon-dachshunds-in-love-vector-id865392634?s=2048x2048",
+  "https://media.gettyimages.com/vectors/heart-shaped-sea-otters-in-love-vector-graphics-vector-id1183276814?s=2048x2048",
+  "https://media.gettyimages.com/vectors/funny-no-prob-llama-vector-illustration-vector-id1089951888?s=2048x2048",
+  "https://media.gettyimages.com/vectors/cute-sloth-sitting-in-lotus-yoga-pose-cartoon-sloth-bear-vector-vector-id1076571820?s=2048x2048",
+  "https://media.gettyimages.com/vectors/hand-drawing-hipster-fantasy-animal-unicorn-illustration-vector-id1065322868?s=2048x2048",
+];
 
-// const collection = document.getElementsByClassName("empty");
-// const emptyPuzzle = collection[0];
+let root = document.documentElement;
+root.style.setProperty(
+  "--image",
+  "url(" + img[(Math.random() * img.length) | 0] + ")"
+);
 
-document.addEventListener("dragend", function (ev) {
-  console.log(ev);
-});
-document.addEventListener("drop", function (event) {
-  event.preventDefault();
+let dragged = null;
+
+document.addEventListener("dragstart", function (event) {
   console.log(event);
+  dragged = event.target;
 });
+
+//Включаем события по сбрасыванию элемента
+document.addEventListener("dragover", function (event) {
+  event.preventDefault();
+});
+
+document.addEventListener("drop", function (event) {
+  const dropped = event.target;
+  if (dropped.className == dragged.className) {
+    dropped.className += " show";
+    dragged.className += " hide";
+    console.log(event);
+  }
+});
+
+let draggableElements = document.querySelector(".draggable");
+for (var i = draggableElements.children.length; i >= 0; i--) {
+  draggableElements.appendChild(
+    draggableElements.children[(Math.random() * i) | 0]
+  );
+}
+console.log(draggableElements);
